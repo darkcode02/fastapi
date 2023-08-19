938392185feff5a1ab7e340431681cb498d0bd2e
dark <tupabloarias12345@gmail.com>
2023-08-18 19:22:23 -0600

    cambios en el html y css

--- main.py
+++ main.py
@@ -39,9 +39,77 @@ movies = [
 ]
 
 # Ruta para mostrar un mensaje en la página de inicio
-@app.get('/', tags=['home'])
+@app.get('/', response_class=HTMLResponse, tags=['home'])
 def message():
-    return HTMLResponse('<h1>Hello world<h1/>')
+    return """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        min-height: 100vh;
+        background-color: #f2f2f2;
+      }
+      .container {
+        background-color: #fff;
+        padding: 20px;
+        border-radius: 8px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        width: 80%;
+        max-width: 600px;
+      }
+      h1 {
+        font-size: 24px;
+        margin-bottom: 10px;
+      }
+      p {
+        font-size: 16px;
+        line-height: 1.5;
+      }
+      .method {
+        font-weight: bold;
+        color: #3498db;
+      }
+    </style>
+    <title>FastAPI - Métodos HTTP</title>
+    </head>
+    <body>
+    <div class="container">
+      <h1>Explorando Métodos HTTP en FastAPI</h1>
+      <p>FastAPI es un marco web de Python que permite crear API REST de manera eficiente. A continuación, se explican los métodos HTTP más comunes:</p>
+      
+      <div class="method">
+        <h2>GET</h2>
+        <p>El método GET se utiliza para solicitar datos de un recurso específico. Es seguro y no debe cambiar el estado del servidor.</p>
+      </div>
+      
+      <div class="method">
+        <h2>POST</h2>
+        <p>El método POST se utiliza para enviar datos al servidor para crear un nuevo recurso. Puede cambiar el estado del servidor.</p>
+      </div>
+      
+      <div class="method">
+        <h2>PUT</h2>
+        <p>El método PUT se utiliza para actualizar un recurso existente en el servidor con los datos proporcionados. Debería ser idempotente.</p>
+      </div>
+      
+      <div class="method">
+        <h2>DELETE</h2>
+        <p>El método DELETE se utiliza para eliminar un recurso del servidor. Debería ser idempotente y seguro.</p>
+      </div>
+    </div>
+    </body>
+    </html>
+    """
+
 
 # Ruta para obtener la lista de películas
 @app.get('/movies', tags=['movies'])
